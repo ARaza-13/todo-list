4a62d2007c3c388d076e58d9d3f73482997a43c1
Ali Raza <aliraza021419@gmail.com>
2023-08-18 19:26:35 -0400

    Created a project class module where users can create new project lists and add their todo tasks into

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import Task from "./task";
+import Project from "./project";
 
-const gym = new Task("Gym");
-console.log(gym.getName());
-console.log(gym.getDate());
-console.log(gym.getDescription());
-console.log(gym.getPriority());
-gym.setName("Push Day");
-gym.setDate("8/21/2023");
-gym.setDescription("Train chest and triceps from 9:45pm to 11:00pm");
-gym.setPriority("High");
-console.log(gym.getName());
-console.log(gym.getDate());
-console.log(gym.getDescription());
-console.log(gym.getPriority());
+const task = new Task("Gym");
+task.setName("Push Day");
+task.setDate("8/21/2023");
+task.setDescription("Train chest and triceps from 9:45pm to 11:15pm");
+task.setPriority("High");
+console.log(task.getName());
+console.log(task.getDate());
+console.log(task.getDescription());
+console.log(task.getPriority());
+
+const project = new Project();
+project.setName("Gym");
+project.addTask(task);
+console.log(project.getName());
+console.log(project.getTasks());
+console.log(project.getTask("Push Day"));
+
+project.deleteTask("Push Day");
+console.log(project.getTasks());
+console.log(project.getTask("Push Day"));
+
+const task2 = new Task();
+task2.setName("Pull Day");
+task2.setDescription("Train back and biceps from 9:45pm to 11:15pm");
+task2.setPriority("High");
+task2.setDate("8/23/2023");
+
+const task3 = new Task();
+task3.setName("Leg Day");
+task3.setDescription("Train legs from 9:45pm to 11:00pm");
+task3.setPriority("High");
+task3.setDate("8/25/2023");
+
+project.setTasks([task, task2, task3]);
+console.log(project.getTasks());
+console.log(project.getTask("Push Day"));
+console.log(project.getTask("Pull Day"));
+console.log(project.getTask("Leg Day"));
