1dfd84e83d0fe9044862009188a7b887b06daeeb
Ali Raza <aliraza021419@gmail.com>
2023-08-24 22:36:55 -0400

    Created a renderProject method in the DOMManager class to load all the created projects on the webpage

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,20 @@
 class DOMManager {
-    constructor() {
+    constructor(todoList, Project, Task) {
+        this.todoList = todoList;
+        this.Project = Project;
+        this.Task = Task;
+
         this.projectsContainer = document.getElementById('projects');
         this.tasksConatiner = document.getElementById('tasks');
     }
+
+    renderProject(project) {
+        const projectConatiner = document.createElement('div');
+        projectConatiner.classList.add('project');
+        projectConatiner.textContent = project.name;
+
+        this.projectsContainer.appendChild(projectConatiner);
+    }
 }
+
+export default DOMManager
--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import Task from "./task";
 import Project from "./project";
 import TodoList from "./todolist";
+import DOMManager from "./dom";
+
+const project = new Project();
+project.setName("Gym");
 
 const task = new Task("Gym");
 task.setName("Push Day");
 task.setDate("8/21/2023");
 task.setDescription("Train chest and triceps from 9:45pm to 11:15pm");
 task.setPriority("High");
-console.log(task.getName());
-console.log(task.getDate());
-console.log(task.getDescription());
-console.log(task.getPriority());
-
-const project = new Project();
-project.setName("Gym");
-project.addTask(task);
-console.log(project.getName());
-console.log(project.getTasks());
-console.log(project.getTask("Push Day"));
-
-project.deleteTask("Push Day");
-console.log(project.getTasks());
-console.log(project.getTask("Push Day"));
 
 const task2 = new Task();
 task2.setName("Pull Day");
@@ -39,9 +28,6 @@ project.addTask(task);
 project.addTask(task2);
 project.addTask(task3);
 console.log(project.getTasks());
-console.log(project.getTask("Push Day"));
-console.log(project.getTask("Pull Day"));
-console.log(project.getTask("Leg Day"));
 
 const trophyTask = new Task();
 trophyTask.setName('The Last of Us Part 2');
@@ -72,7 +58,7 @@ console.log(todoList.getProjects());
 todoList.addProject(project);
 todoList.addProject(trophyHunting);
 todoList.addProject(coding);
-console.log(todoList.getProjects());
-console.log(todoList.getProject(project.getName()));
-console.log(todoList.getProject(trophyHunting.getName()));
-console.log(todoList.getProject(coding.getName()));
+
+const domManager = new DOMManager(todoList, Project, Task);
+
+todoList.getProjects().forEach((project) => domManager.renderProject(project));
