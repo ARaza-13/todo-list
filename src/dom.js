e7ffabfd994807ab964b52a504d71b75fc307092
Ali Raza <aliraza021419@gmail.com>
2023-08-24 22:51:36 -0400

    Created a renderTasks method to load all tasks assigned to whichever project is clicked

--- src/dom.js
+++ src/dom.js
@@ -13,7 +13,25 @@ class DOMManager {
         projectConatiner.classList.add('project');
         projectConatiner.textContent = project.name;
 
+        projectConatiner.addEventListener('click', () => this.renderTasks(project));
+
         this.projectsContainer.appendChild(projectConatiner);
+    }
+
+    renderTasks(project) {
+        this.clearTasks();
+
+        project.getTasks().forEach((task) => {
+            const taskContainer = document.createElement('div');
+            taskContainer.classList.add('task');
+            taskContainer.textContent = task.getName();
+
+            this.tasksConatiner.appendChild(taskContainer);
+        });
+    }
+
+    clearTasks() {
+        this.tasksConatiner.innerHTML = '';
     }
 }
 
