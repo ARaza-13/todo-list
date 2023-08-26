4ca326e8c4c71e8a8907cb31c2c08605f5423335
Ali Raza <aliraza021419@gmail.com>
2023-08-25 20:26:51 -0400

    Added a method to render the task details when its corresponding task is clicked

--- src/dom.js
+++ src/dom.js
@@ -3,6 +3,8 @@ class DOMManager {
         this.todoList = todoList;
         this.Project = Project;
         this.Task = Task;
+        this.taskDetailsContainer = document.createElement('div');
+        this.taskDetailsContainer.classList.add('todo-details');
 
         this.projectsContainer = document.getElementById('projects');
         this.tasksConatiner = document.getElementById('tasks');
@@ -26,12 +28,41 @@ class DOMManager {
             taskContainer.classList.add('task');
             taskContainer.textContent = task.getName();
 
+            taskContainer.addEventListener('click', () => this.renderTaskDetails(task));
+
             this.tasksConatiner.appendChild(taskContainer);
         });
     }
 
+    renderTaskDetails(task) {
+        this.clearTaskDetails();
+
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = task.getName();
+
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = task.getDescription();
+
+        const dueDateElement = document.createElement('p');
+        dueDateElement.textContent = `Due: ${task.getDate()}`;
+
+        const priorityElement = document.createElement('p');
+        priorityElement.textContent = `Priority: ${task.getPriority()};`;
+
+        this.taskDetailsContainer.appendChild(titleElement);
+        this.taskDetailsContainer.appendChild(descriptionElement);
+        this.taskDetailsContainer.appendChild(dueDateElement);
+        this.taskDetailsContainer.appendChild(priorityElement);
+
+        this.tasksConatiner.appendChild(this.taskDetailsContainer);
+    }
+
     clearTasks() {
         this.tasksConatiner.innerHTML = '';
+    }
+
+    clearTaskDetails() {
+        this.taskDetailsContainer.innerHTML = '';
     }
 }
 
