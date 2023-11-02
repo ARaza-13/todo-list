c27a4ad228a6d8daf82e6257ed027af81ee9380c
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2023-11-01 23:13:43 -0400

    Created a storage.js module with a Storage class and constructor

---
+++ src/storage.js
@@ -0,0 +1,9 @@
+import Task from "./task";
+import Project from "./project";
+import TodoList from "./todolist";
+
+class Storage {
+    constructor() {
+        this.localStorageKey = 'todoList';
+    }
+}
