0f37528333e4cd4e7cacbfcdef914081ec6e32c4
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2023-12-06 16:04:11 -0500

    Made a create nav method in the create-html file

--- src/create-html.js
+++ src/create-html.js
@@ -1,3 +1,13 @@
 class CreateHtml {
-    
+    constructor() {
+        this.main = document.querySelector('main');
+    }
+
+    createNav() {
+        const nav = document.createElement('div');
+        nav.classList.add('nav');
+        nav.setAttribute('id', 'nav');
+
+        return nav;
+    }
 }
